2244bd93f1a75db75bdc653a24074400b916574b
PetarPeychev <petarpeychev98@gmail.com>
2018-05-12 14:52:46 +0300

    ex 12

---
+++ 03-expressions-conditionals-loops/12-assign-properties.js
@@ -0,0 +1,7 @@
+function composeObject([prop1, val1, prop2, val2, prop3, val3]) {
+    let object = {};
+    object[prop1] = val1;
+    object[prop2] = val2;
+    object[prop3] = val3;
+    return object;
+}
